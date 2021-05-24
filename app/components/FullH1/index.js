3d9ed690bba8755745658abb69c4f88684fbcb0f
Success Malla <success_malla@hotmail.com>
2021-05-24 22:25:40 +0545

    rollback

--- app/components/FullH1/index.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const FullH1 = styled.h1`
-  font-size: 4em;
-  color: #647b84;
-`;
-
-export default FullH1;
--- app/components/Language/index.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Language = styled.span`
-  position: fixed;
-  top: 10px;
-  right: 45px;
-`;
-
-export default Language;
