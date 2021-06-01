b617acba51d3f450f9cc5868404e5102e66d9db4
Success Malla <success_malla@hotmail.com>
2021-06-01 22:43:31 +0545

    v1 finalized

---
+++ 0.87d300c2a26aaba397d0.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{"1fa8ea4b329e73f4b9a6":function(a,i,n){"use strict";var o=n("0b3cb19af78752326f59").b.div.withConfig({displayName:"FullHeight",componentId:"sc-1wa66np-0"})(["min-height:calc(100vh);padding-top:10%;padding-bottom:10%;"]);i.a=o}}]);
