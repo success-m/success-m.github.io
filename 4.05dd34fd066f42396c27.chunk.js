9c330dc3257bd5bc449949c5e6d8cee9aeedaa07
Success Malla <success_malla@hotmail.com>
2021-06-07 22:20:45 +0545

    v2 changes

---
+++ 4.05dd34fd066f42396c27.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"9a2d5dc5295a73b27dbf":function(d,p,n){d.exports=n.p+"b15453488d14cc0864fd3a18d859bcf3.png"},f6d2bb76f091d23de360:function(d,p,n){d.exports=n.p+"684b9632063d924d092dce9553118dfe.png"}}]);
